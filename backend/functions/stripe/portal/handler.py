a372e32c5d9a2e2ba5a85df269b186a96a5c54fa
TenClay, Ethan <ethantenclay@my.unt.edu>
2026-04-09 01:03:36 -0500

    dont need alt paths

--- backend/functions/stripe/portal/handler.py
+++ backend/functions/stripe/portal/handler.py
@@ -9,9 +9,6 @@ Users are redirected there to cancel, update payment, or manage their subscripti
 import os
 import sys
 
-sys.path.insert(0, "/opt/shared")
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../../../../shared"))
-
 import db
 import security as sec
 
